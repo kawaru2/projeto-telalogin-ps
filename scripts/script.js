30a5c85dec720361e3af03c1e85765fdfc778bce
kawaru2 <joaopedrocb2@gmail.com>
2023-02-27 13:21:59 -0300

    ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 function menu() { // MENU HAMBURGUER
     if (items.style.display == `flex`) {
         items.style.display = `none`;
+        items.style.animation = `display .3s`;
     } else {
         items.style.display = `flex`;
+        items.style.transition = `display .3s`;
     }
 }
